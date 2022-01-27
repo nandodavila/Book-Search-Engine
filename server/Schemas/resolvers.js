e527659691dcfe8deb1355ce55c85ea1ba4f3365
Nando <nandodavila94@gmail.com>
2022-01-27 18:53:43 -0500

    just trying to commit more

---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,45 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        user: async (parent, { user }) => {
+            return User.findOne({
+                $or: [
+                    { _id: user ? user._id : params.id }, 
+                    { username: params.username }
+                ],
+            });
+        },
+    },
+
+  Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('No user found with this email address');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+
+    
+  },
+};
+
+module.exports = resolvers;
